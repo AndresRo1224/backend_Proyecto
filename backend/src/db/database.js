5866e61f01e20659ca5451f3d183800238c1cbe5
Andres Rangel <andresrangel8a@gmail.com>
2025-05-06 15:08:01 -0500

    Arreglos Basicos

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,16 +1,20 @@
-import mysql from "promise-mysql";
-
+import mysql from "mysql2/promise";
 import config from "../config.js";
 
-const connection = mysql.createConnection({
+// Crear la conexión a la base de datos
+const connection = mysql.createPool({
   host: config.host,
   user: config.user,
   password: config.password,
   database: config.database,
-});  
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
+// Función para obtener la conexión
 const getConnection = () => {
   return connection;
-}
+};
 
 export default getConnection;
