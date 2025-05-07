e51505b687ac21c6b013d719d702ff2902bcf60f
Andres Rangel <andresrangel8a@gmail.com>
2025-05-07 12:03:17 -0500

    Arreglo general

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,18 +1,14 @@
 import mysql from "mysql2/promise";
+
 import config from "../config.js";
 
-// Crear la conexión a la base de datos
 const connection = mysql.createPool({
   host: config.host,
   user: config.user,
   password: config.password,
   database: config.database,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-// Función para obtener la conexión
 const getConnection = () => {
   return connection;
 };
